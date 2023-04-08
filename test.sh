20bd8c0141de12586e956725701b1087cdfe55db
Damodaran G <damodaran_g@yahoo.com>
2023-04-09 08:13:51 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-echo "Tesing my build..."
-SF = ${buildkite-agent meta-data get "TEST_ENV"}
-echo $SF
+echo "Testing my build..."
+buildkite-agent meta-data get "TEST_ENV"
+echo $TEST_ENV
 buildkite-agent artifact upload ./test.sh
