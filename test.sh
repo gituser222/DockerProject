a403e972d0f2cada690b6bfadff4cec69ad62627
Damodaran G <damodaran_g@yahoo.com>
2023-04-15 10:04:43 +1000

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,3 +3,7 @@ buildkite-agent meta-data get "TEST_ENV"
 echo $TEST_ENV
 docker -v
 docker run hello-world
+echo "Building docker image"
+docker build --tag test .
+echo "Runnung docker image"
+docker run -rm test
